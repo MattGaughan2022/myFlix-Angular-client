8773b7390c25ad8b599d1bfb13e23aa4c2134218
Matt Gaughan <mattgaugs@gmail.com>
2023-10-18 18:38:57 -0400

    ghpages deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// finally able to ghpage deploy with
+// ng build --base-href "https://mattgaughan2022.github.io/myFlix-Angular-client/"
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
